377dc0bae2ac8b1060b9df3bd4fe7441558ab223
Albin18 <albianjose0424@gmail.com>
2024-05-20 04:26:33 -0400

    projects

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,30 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-window:any;
- constructor(){ }
-
- ngOnInit(): void {
-  window.onscroll = () => {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("header nav a");
-    const top = window.scrollY;
-
-    sections.forEach((sec: Element) => {
-      const offset = (sec as HTMLElement).offsetTop - 150;
-      const height = (sec as HTMLElement).offsetHeight;
-      const id = sec.getAttribute("id");
-
-      if (top >= offset && top < offset + height) {
-        navLinks.forEach((links: Element) => {
-          links.classList.remove("active");
-          document
-            .querySelector(`header nav a[href*="${id}"]`)
-            ?.classList.add("active");
-        });
-      }
-    });
-  };
-}
 
 }
